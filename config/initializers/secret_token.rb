c3a29ff3d095a5f3a024032877b3d4ff20d102c5
zubygit <zubair_magrey@hotmail.co.uk>
2014-04-27 14:48:33 +0100

    Initialise repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hunt::Application.config.secret_key_base = '6f8ff445d92ab2ecd8875066d1600270340fda9b7fbb8e79e154f0e2a88867f11b30b997c7da9210c60a0b1a0c7ad53d4d254549994844cbf07c28b645c2f40b'
