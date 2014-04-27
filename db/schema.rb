5bc751d6e3adda5355332454d175fbbf39c69bab
zubygit <zubair_magrey@hotmail.co.uk>
2014-04-27 21:01:05 +0100

    adding secure passwords to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140427145153) do
+ActiveRecord::Schema.define(version: 20140427194924) do
 
   create_table "requests", force: true do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140427145153) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
