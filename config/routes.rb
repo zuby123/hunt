f871ad9052c2cbda0e13e5e0cab697dedda87178
zubygit <zubair_magrey@hotmail.co.uk>
2014-05-04 10:49:22 +0100

    finishing sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Hunt::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   
   resources :requests
 
