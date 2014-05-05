bd6592ba2666799d43bc7565c872c8b221afd675
zubygit <zubair_magrey@hotmail.co.uk>
2014-05-05 14:27:13 +0100

    Finish user edits, update, index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+ before_action :signed_in_user, only: [:edit, :update]
+ before_action :correct_user,   only: [:edit, :update]
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   # GET /users/1
@@ -44,14 +45,11 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -75,5 +73,17 @@ class UsersController < ApplicationController
     
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+     def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
